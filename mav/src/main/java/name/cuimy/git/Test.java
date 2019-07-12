17be5064f0470b4d2a609a112dca9d7f21ffa5c0
Administrator <1179911433@qq.com>
2019-07-12 22:40:43 +0800

    init

---
+++ mav/src/main/java/name/cuimy/git/Test.java
@@ -0,0 +1,12 @@
+package name.cuimy.git;
+
+/**
+ * Created by Administrator on 2019/7/12.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("11111111111");
+        System.out.println("123");
+        System.out.println("121113");
+    }
+}
